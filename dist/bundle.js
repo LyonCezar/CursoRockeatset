9c62228304dd80c43f5774fb41b430db018e714a
LyonCezar <lyoncezarborges@gmail.com>
2022-02-21 09:47:48 -0300

    adicionando babel e webpack

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,9 +1,32 @@
-"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
 
-var _react = _interopRequireDefault(require("react"));
+/***/ "./src/index.jsx":
+/*!***********************!*\
+  !*** ./src/index.jsx ***!
+  \***********************/
+/***/ (() => {
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: C:\\\\github\\\\Projeto-Ignite\\\\src\\\\index.jsx: Identifier 'React' has already been declared. (2:7)\\n\\n\\u001b[0m \\u001b[90m 1 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[33mReact\\u001b[39m \\u001b[36mfrom\\u001b[39m \\u001b[32m'react'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 2 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[33mReact\\u001b[39m \\u001b[36mfrom\\u001b[39m \\u001b[32m'react-dom'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m   |\\u001b[39m        \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 3 |\\u001b[39m \\u001b[36mimport\\u001b[39m { render } \\u001b[36mfrom\\u001b[39m \\u001b[32m'react-dom'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 4 |\\u001b[39m \\u001b[36mimport\\u001b[39m { \\u001b[33mApp\\u001b[39m } \\u001b[36mfrom\\u001b[39m \\u001b[32m'./App'\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 5 |\\u001b[39m\\u001b[0m\\n    at Object._raise (C:\\\\github\\\\Projeto-Ignite\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:476:17)\\n    at Object.raiseWithData (C:\\\\github\\\\Projeto-Ignite\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:469:17)\\n    at Object.raise (C:\\\\github\\\\Projeto-Ignite\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:430:17)\\n    at ScopeHandler.checkRedeclarationInScope (C:\\\\github\\\\Projeto-Ignite\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:1720:12)\\n    at ScopeHandler.declareName (C:\\\\github\\\\Projeto-Ignite\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:1686:12)\\n    at Object.checkLVal (C:\\\\github\\\\Projeto-Ignite\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:11626:24)\\n    at Object.parseImportSpecifierLocal (C:\\\\github\\\\Projeto-Ignite\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:15735:10)\\n    at Object.maybeParseDefaultImportSpecifier (C:\\\\github\\\\Projeto-Ignite\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:15843:12)\\n    at Object.parseImport (C:\\\\github\\\\Projeto-Ignite\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:15700:31)\\n    at Object.parseStatementContent (C:\\\\github\\\\Projeto-Ignite\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:14223:27)\");\n\n//# sourceURL=webpack://projeto-ignite/./src/index.jsx?");
 
-function App() {
-  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World");
-}
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.jsx"]();
+/******/ 	
+/******/ })()
+;
